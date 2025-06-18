92d500993aaa6130706d58e687f9462634247e3d
da200274d <da200274d@student.etf.bg.ac.rs>
2025-06-19 01:10:37 +0200

    init: Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Guestbook</h1>
+      <p>See what people wrote about us and feel free to leave a message. </p>
+    </div>
+  );
+}
+
+export default App;
