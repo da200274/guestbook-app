dce12edb041e2ff5e1025798592bd5d503d81484
da200274d <da200274d@student.etf.bg.ac.rs>
2025-06-19 02:09:01 +0200

    feat: connecting backend and database with plain results

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import db from './db.js'
 
 const app = express()
-
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req,res) => {
-    res.send("Hello world!")
+    //res.send("Hello world!")
+    db.query('select 1 + 1 as solution', (err, results)=>{
+        if(err) return res.status(500).json({error: err.message});
+        res.json({result: results});
+    })
 })
 
 app.listen(4000, ()=>console.log('Express running on port 4000'))
