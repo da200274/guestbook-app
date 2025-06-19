618942f2a7c23edade1afd4d533f6271fb1cd318
da200274d <da200274d@student.etf.bg.ac.rs>
2025-06-19 13:34:36 +0200

    feat: post request for sending message

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req,res) => {
+app.get('/getMessage', (req,res) => {
     //res.send("Hello world!")
     db.query('select 1 + 1 as solution', (err, results)=>{
         if(err) return res.status(500).json({error: err.message});
@@ -15,5 +15,23 @@ app.get('/', (req,res) => {
         res.json({result: data[0].solution});
     })
 })
+
+app.post('/sendMessage', (req, res) => {
+    const {name, content} = req.body;
+
+    if(!name || !content){
+        res.status(400).json({error: "Name and message is required"});
+    }
+    else{
+        const sql = 'insert into Message (name, message) values (?, ?)';
+        db.query(sql, [name, content], (err, results)=>{
+            if(err) return res.status(500).json({error: err.message});
+
+            res.status(201).json({success: true, id: results})
+        })
+    }
+})
+
+export default app;
 
 app.listen(4000, ()=>console.log('Express running on port 4000'))
